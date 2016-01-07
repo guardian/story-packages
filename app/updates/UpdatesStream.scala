540e5ceb5403129499c6ceff43d3242034cc1d4e
Reetta Vaahtoranta <reetta.vaahtoranta@guardian.co.uk>
2016-01-07 12:43:49 +0000

    only push updates to capi stream if package is not hidden

--- app/updates/UpdatesStream.scala
+++ app/updates/UpdatesStream.scala
@@ -1,9 +1,23 @@
 package updates
 
+import services.Database
+import play.api.libs.concurrent.Execution.Implicits._
+
 object UpdatesStream {
 
   def putStreamUpdate(streamUpdate: StreamUpdate): Unit = {
+
     AuditingUpdates.putStreamUpdate(streamUpdate)
-    KinesisEventSender.putCapiUpdate(streamUpdate.collections)
+
+    streamUpdate.collections.foreach(keyValue => {
+      for {
+        storyPackage <- Database.getPackage(keyValue._1)
+      } yield {
+        storyPackage.isHidden match {
+          case Some(false) => KinesisEventSender.putCapiUpdate(streamUpdate.collections)
+          case _ =>
+        }
+      }
+    })
   }
 }
