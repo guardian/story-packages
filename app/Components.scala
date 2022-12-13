4e4e5063fbfe613debf2ca895ad791074f07407f
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2022-12-13 13:15:25 +0000

    Remove custom assets handling

--- app/Components.scala
+++ app/Components.scala
@@ -13,7 +13,7 @@ import conf.{ApplicationConfiguration, Responses}
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.filters.gzip.GzipFilter
 
-class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with AhcWSComponents  {
+class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with AhcWSComponents {
   val isTest = context.environment.mode == Mode.Test
   val isDev = context.environment.mode == Mode.Dev
   val config = new ApplicationConfiguration(configuration, context.environment.mode)
@@ -38,7 +38,6 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
   val pandaAuth = new PandaAuthController(config, wsClient)
   val status = new StatusController
   val storyPackages = new StoryPackagesController(config, database, updatesStream, frontsApi, reindex, wsClient)
-  val uncachedAssets = new UncachedAssets
   val vanity = new VanityRedirects(config, wsClient)
   val views = new ViewsController(config, assetsManager, wsClient)
 
@@ -49,5 +48,7 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
     new CORSFilter
   )
 
-  val router: Router = new Routes(httpErrorHandler, status, pandaAuth, uncachedAssets, views, faciaTool, defaults, storyPackages, faciaProxy, vanity)
+  val assets = new Assets(httpErrorHandler)
+
+  val router: Router = new Routes(httpErrorHandler, status, pandaAuth, views, faciaTool, defaults, storyPackages, faciaProxy, vanity, assets)
 }
