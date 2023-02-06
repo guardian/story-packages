7570404d6dc2c37326bf69296aceb05118ff4b91
Jonathon Herbert <jonathon.herbert@guardian.co.uk>
2023-02-06 13:49:59 +0000

    Changes for pan-domain-auth 0.7.2

--- app/story_packages/permissions/APIKeyAuthAction.scala
+++ app/story_packages/permissions/APIKeyAuthAction.scala
@@ -1,13 +1,13 @@
 package story_packages.permissions
 
-import play.api.mvc.{ActionBuilder, Request, Result}
+import play.api.mvc.{ActionBuilder, AnyContent, Request, Result}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import play.api.mvc.Results.Forbidden
 import conf.ApplicationConfiguration
 
-class APIKeyAuthAction(config: ApplicationConfiguration) extends ActionBuilder[Request] {
+class APIKeyAuthAction(config: ApplicationConfiguration) extends ActionBuilder[Request, AnyContent] {
   override def invokeBlock[A](request: Request[A], block: (Request[A]) => Future[Result]): Future[Result] = {
     request.queryString.getOrElse("api-key", Nil) match {
       case Seq(config.reindex.key) => block(request)
