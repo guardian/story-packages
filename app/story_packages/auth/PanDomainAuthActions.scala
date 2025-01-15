b03e1178d82a382b3167aece6771cad15943848d
Andrew Nowak <andrew.nowak@guardian.co.uk>
2025-01-15 16:03:34 +0000

    Add unauthzed message

--- app/story_packages/auth/PanDomainAuthActions.scala
+++ app/story_packages/auth/PanDomainAuthActions.scala
@@ -33,10 +33,12 @@ trait PanDomainAuthActions extends AuthActions with Results with Logging {
 
   override def invalidUserMessage(claimedAuth: AuthenticatedUser): String = {
     if( (claimedAuth.user.emailDomain == "guardian.co.uk") && !claimedAuth.multiFactor) {
-      s"${claimedAuth.user.email} is not valid for use with the Fronts Tool as you need to have two factor authentication enabled." +
-       s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request access to Composer CMS tools."
-    } else {
+      s"${claimedAuth.user.email} is not valid for use with the Story Packages tool as you need to have two factor authentication enabled." +
+       s" Please contact the Helpdesk by emailing 34444@theguardian.com or calling 34444 and request assistance setting up two factor authentication on your Google account."
+    } else if (claimedAuth.user.emailDomain != "guardian.co.uk") {
       s"${claimedAuth.user.email} is not valid for use with the Fronts Tool. You need to use your Guardian Google account to login. Please sign in with your Guardian Google account first, then retry logging in."
+    } else {
+      s"${claimedAuth.user.email} has not been granted access to the Story Packages tool. Please contact Central Production at central.production@guardian.co.uk requesting access to the Story Packages tool."
     }
   }
 }
