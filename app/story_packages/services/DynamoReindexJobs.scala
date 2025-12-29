0fe5c78a2d2cba1445a806d813066acb01d4f41e
Andy Noton <andy.noton.contractor@guardian.co.uk>
2025-12-29 11:45:19 +0000

    dynamodb reindex

--- app/story_packages/services/DynamoReindexJobs.scala
+++ app/story_packages/services/DynamoReindexJobs.scala
@@ -1,30 +1,42 @@
 package story_packages.services
 
-import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
-import com.amazonaws.services.dynamodbv2.{AmazonDynamoDBClientBuilder}
-import com.amazonaws.services.dynamodbv2.document.spec.{DeleteItemSpec, QuerySpec, ScanSpec, UpdateItemSpec}
-import com.amazonaws.services.dynamodbv2.document.utils.ValueMap
-import com.amazonaws.services.dynamodbv2.document.{AttributeUpdate, DynamoDB, Item}
 import story_packages.metrics.ReindexMetrics
 import conf.ApplicationConfiguration
+import software.amazon.awssdk.enhanced.dynamodb.{AttributeConverterProvider, AttributeValueType, DynamoDbEnhancedClient, Expression, Key, TableMetadata, TableSchema}
+import software.amazon.awssdk.enhanced.dynamodb.document.EnhancedDocument
+import software.amazon.awssdk.enhanced.dynamodb.model.{QueryConditional, QueryEnhancedRequest, ScanEnhancedRequest}
+import software.amazon.awssdk.regions.Region
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue
+import story_packages.services.DynamoReindexJobs.jobAsItem
 import story_packages.updates._
 
 import scala.jdk.CollectionConverters._
 
 class DynamoReindexJobs(config: ApplicationConfiguration) extends Logging {
   private lazy val client =
-    AmazonDynamoDBClientBuilder.standard
-      .withCredentials(config.aws.mandatoryCredentials)
-      .withEndpointConfiguration(new EndpointConfiguration(config.aws.endpoints.dynamoDB, config.aws.region))
-      .build
+    DynamoDbClient.builder()
+      .credentialsProvider(config.awsV2.mandatoryCredentials)
+      .region(Region.of(config.awsV2.region))
+      .endpointOverride(config.awsV2.endpoints.dynamoDB)
+      .build()
 
-  private lazy val table = new DynamoDB(client).getTable(config.reindex.progressTable)
+  private lazy val enhancedClient = DynamoDbEnhancedClient.builder()
+    .dynamoDbClient(client)
+    .build();
 
-  private def asReindexProgress(item: Item): ReindexProgress = {
+  private lazy val table = enhancedClient.table(config.storage.configTable,
+    TableSchema.documentSchemaBuilder()
+      .addIndexPartitionKey(TableMetadata.primaryIndexName(), "reindexStatus", AttributeValueType.S)
+      .addIndexSortKey(TableMetadata.primaryIndexName(), "startTime", AttributeValueType.S)
+      .attributeConverterProviders(AttributeConverterProvider.defaultProvider())
+      .build())
+
+  private def asReindexProgress(item: EnhancedDocument): ReindexProgress = {
     ReindexProgress(
       status = item.getString("reindexStatus"),
-      documentsIndexed = item.getInt("documentsIndexed"),
-      documentsExpected = item.getInt("documentsExpected")
+      documentsIndexed = item.getNumber("documentsIndexed").intValue(),
+      documentsExpected = item.getNumber("documentsExpected").intValue()
     )
   }
 
@@ -33,28 +45,27 @@ class DynamoReindexJobs(config: ApplicationConfiguration) extends Logging {
   }
 
   def jobInProgress(isHidden: Boolean): Option[ReindexProgress] = {
-    val values = new ValueMap()
-      .withString(":status", "in progress")
-      .withBoolean(":hidden", isHidden)
 
-    val queryExpression = new QuerySpec()
-      .withKeyConditionExpression("reindexStatus = :status")
-      .withFilterExpression("isHidden = :hidden")
-      .withValueMap(values)
-      .withMaxResultSize(1)
+    val statusInProgress = Key.builder().partitionValue("in progress").build()
 
-    val job = table.query(queryExpression).asScala.toList.map(asReindexProgress).headOption
+    val isHiddenExpression = Expression.builder()
+      .expression("isHidden = :is_hidden")
+      .putExpressionValue(":is_hidden", AttributeValue.builder().bool(isHidden).build())
+      .build();
+
+    val queryExpression = QueryEnhancedRequest.builder()
+      .queryConditional(QueryConditional.keyEqualTo(statusInProgress))
+      .filterExpression(isHiddenExpression)
+      .build()
+
+    val job = table.query(queryExpression).items().asScala.toList.map(asReindexProgress).headOption
     ReindexMetrics.QueryCount.increment()
     job
   }
 
   def createJob(reindexPage: ReindexPage): RunningJob = {
     val job = RunningJob(reindexPage)
-    val item = new Item()
-      .withPrimaryKey("reindexStatus", job.status.label, "startTime", job.startTime.toString)
-      .withInt("documentsIndexed", job.documentsIndexed)
-      .withInt("documentsExpected", job.documentsExpected)
-      .withBoolean("isHidden", job.isHidden)
+    val item = jobAsItem(job)
 
     Logger.info(s"Creating reindex job at ${job.startTime}")
     table.putItem(item)
@@ -64,14 +75,10 @@ class DynamoReindexJobs(config: ApplicationConfiguration) extends Logging {
 
   def markProgressUpdate(previousRunningJob: RunningJob, processedResults: Int) = {
     val job = previousRunningJob.copy(documentsIndexed = processedResults)
-    val updateSpec = new UpdateItemSpec()
-      .withPrimaryKey("reindexStatus", job.status.label, "startTime", job.startTime.toString)
-      .addAttributeUpdate(new AttributeUpdate("documentsIndexed").put(job.documentsIndexed))
-      .addAttributeUpdate(new AttributeUpdate("documentsExpected").put(job.documentsExpected))
-      .addAttributeUpdate(new AttributeUpdate("isHidden").put(job.isHidden))
+    val updatedItem = jobAsItem(job)
 
     Logger.info(s"Marking reindex progress update at ${job.startTime}")
-    table.updateItem(updateSpec)
+    table.updateItem(updatedItem)
     ReindexMetrics.UpdateCount.increment()
   }
 
@@ -82,17 +89,10 @@ class DynamoReindexJobs(config: ApplicationConfiguration) extends Logging {
     )
 
     Logger.info(s"Marking reindex complete at ${previousRunningJob.startTime}")
-    val item = new Item()
-      .withPrimaryKey("reindexStatus", job.status.label, "startTime", job.startTime.toString)
-      .withInt("documentsIndexed", job.documentsIndexed)
-      .withInt("documentsExpected", job.documentsExpected)
-      .withBoolean("isHidden", job.isHidden)
-    table.putItem(item)
+    table.putItem(jobAsItem(job))
     ReindexMetrics.UpdateCount.increment()
 
-    table.deleteItem(new DeleteItemSpec()
-      .withPrimaryKey("reindexStatus", previousRunningJob.status.label, "startTime", previousRunningJob.startTime.toString)
-    )
+    table.deleteItem(jobAsItem(previousRunningJob))
     ReindexMetrics.DeleteCount.increment()
   }
 
@@ -102,34 +102,39 @@ class DynamoReindexJobs(config: ApplicationConfiguration) extends Logging {
     )
 
     Logger.info(s"Marking reindex failed at ${previousRunningJob.startTime}")
-    val item = new Item()
-      .withPrimaryKey("reindexStatus", job.status.label, "startTime", job.startTime.toString)
-      .withInt("documentsIndexed", job.documentsIndexed)
-      .withInt("documentsExpected", job.documentsExpected)
-      .withBoolean("isHidden", job.isHidden)
-    table.putItem(item)
+    table.putItem(jobAsItem(job))
     ReindexMetrics.UpdateCount.increment()
 
-    table.deleteItem(new DeleteItemSpec()
-      .withPrimaryKey("reindexStatus", previousRunningJob.status.label, "startTime", previousRunningJob.startTime.toString)
-    )
+    table.deleteItem(jobAsItem(previousRunningJob))
     ReindexMetrics.UpdateCount.increment()
   }
 
   def getLastStartedJob(isHidden: Boolean): Option[ReindexProgress] = {
-    val values = new ValueMap()
-      .withString(":status", "in progress")
-      .withBoolean(":hidden", isHidden)
-
     Logger.info(s"Scanning reindex jobs for last started job with isHidden $isHidden")
 
-    val scanRequest = new ScanSpec()
-      .withFilterExpression("isHidden = :hidden and not reindexStatus = :status")
-      .withValueMap(values)
+    val expression = Expression.builder()
+      .expression("isHidden = :hidden and not reindexStatus = :status")
+      .putExpressionValue(":hidden", AttributeValue.builder().bool(isHidden).build())
+      .putExpressionValue(":status", AttributeValue.builder().s("in progress").build())
+      .build();
+
+    val scanRequest = ScanEnhancedRequest.builder()
+      .filterExpression(expression)
+      .build()
 
     import SortItemsByLastStartTime._
-    val progress = table.scan(scanRequest).asScala.toList.sorted.map(asReindexProgress).headOption
+    val progress = table.scan(scanRequest).items().asScala.toList.sorted.map(asReindexProgress).headOption
     ReindexMetrics.ScanCount.increment()
     progress
   }
+}
+
+object DynamoReindexJobs {
+  def jobAsItem(job: RunningJob): EnhancedDocument = EnhancedDocument.builder()
+      .putString("reindexStatus", job.status.label)
+      .putString("startTime", job.startTime.toString)
+      .putNumber("documentsIndexed", job.documentsIndexed)
+      .putNumber("documentsExpected", job.documentsExpected)
+      .putBoolean("isHidden", job.isHidden)
+      .build()
 }
